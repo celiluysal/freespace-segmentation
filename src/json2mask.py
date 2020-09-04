f43d97ef3fe55aba2ab550bb56491311248629c9
celiluysal <celiluysl15@gmail.com>
2020-09-04 17:51:29 +0300

    read json and parse some variable

--- src/json2mask.py
+++ src/json2mask.py
@@ -1,19 +1,28 @@
+from os import listdir
+from os.path import isfile, join
 import numpy as np
-import cv2
 import json
 import os
+import cv2
 
+
+        
+json_dicts = []
 JSON_DIR  = '../data/jsons'
-json_name = 'ozan.json'
-json_path = os.path.join(JSON_DIR, json_name)
-json_file = open(json_path, 'r')
-json_dict = json.load(json_file)
+file_names = [f for f in listdir(JSON_DIR) if isfile(join(JSON_DIR, f))]
+
+
+for name in file_names:
+    json_name = name
+    json_path = os.path.join(JSON_DIR, json_name)
+    json_file = open(json_path, 'r')
+    json_dict = json.load(json_file)
+    json_dicts.append(json_dict)
+    
 json_objs = json_dict["objects"]
 
 for obj in json_objs:
-    obj_id = obj['classId']
-    if obj_id == 38:
-        # this is my condition that I want
-        print('I like it')
-    else:
-        continue
+    if obj["classId"] == 38:
+        points = obj["points"]
+        exterior = points["exterior"]
+        print(exterior)
