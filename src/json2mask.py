aa50e0f2c626ee1949b3a2f8ab25ac270349ff88
celiluysal <celiluysl15@gmail.com>
2020-09-04 19:16:37 +0300

    create filled masks

--- src/json2mask.py
+++ src/json2mask.py
@@ -5,34 +5,41 @@ import json
 import os
 import cv2
         
-json_dicts = []
+images = []
 JSON_DIR = "../data/jsons"
 MASK_DIR = "../data/masks"
 
 file_names = [f for f in listdir(JSON_DIR) if isfile(join(JSON_DIR, f))]
 
+class image:
+    def __init__(self, width, height, points, name):
+        self.width = width
+        self.height = height
+        self.points = points
+        self.name = name
 
-for name in file_names:
-    json_name = name
-    json_path = os.path.join(JSON_DIR, json_name)
+for file_name in file_names:
+    json_path = os.path.join(JSON_DIR, file_name)
     json_file = open(json_path, 'r')
     json_dict = json.load(json_file)
-    json_dicts.append(json_dict)
+    width = json_dict["size"]["width"]
+    height = json_dict["size"]["height"]
+    json_objs = json_dict["objects"]
     
-json_objs = json_dict["objects"]
+    for obj in json_objs:
+        if obj["classTitle"] == "Freespace":
+            points = obj["points"]["exterior"]
+            img = image(width, height, points, file_name)
+            images.append(img)
 
-for obj in json_objs:
-    if obj["classTitle"] == "Freespace":
-        points = obj["points"]["exterior"]
-        print(points)
+for image in images:
+    draw_and_save_filledPolygon(image.width, image.height, image.points, image.name)
 
-## draw filled polygon width, height, points 
-mask = np.zeros((1208, 1920, 3), np.uint8)
-pts = np.array(points, np.int32)
-pts = pts.reshape((-1,1,2))
-
-cv2.fillPoly(mask,[pts],(255,255,255))
-cv2.imshow('Window', mask)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite(join(MASK_DIR,"asd.png"),mask)
+## draw filled polygon width, height, points, name
+def draw_and_save_filledPolygon(_width, _height, _points, _name):
+    mask = np.zeros((_width, _height, 3), np.uint8)
+    pts = np.array(_points, np.int32)
+    pts = pts.reshape((-1,1,2))
+    
+    cv2.fillPoly(mask,[pts],(255,255,255))
+    cv2.imwrite(join(MASK_DIR,_name+".png"),mask)
