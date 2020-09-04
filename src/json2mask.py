cf47c90640517e4ed964f36194dc3e7082599bc1
celiluysal <celiluysl15@gmail.com>
2020-09-04 18:42:03 +0300

    draw filled polygon, save image

--- src/json2mask.py
+++ src/json2mask.py
@@ -4,11 +4,11 @@ import numpy as np
 import json
 import os
 import cv2
-
-
         
 json_dicts = []
-JSON_DIR  = '../data/jsons'
+JSON_DIR = "../data/jsons"
+MASK_DIR = "../data/masks"
+
 file_names = [f for f in listdir(JSON_DIR) if isfile(join(JSON_DIR, f))]
 
 
@@ -22,7 +22,17 @@ for name in file_names:
 json_objs = json_dict["objects"]
 
 for obj in json_objs:
-    if obj["classId"] == 38:
-        points = obj["points"]
-        exterior = points["exterior"]
-        print(exterior)
+    if obj["classTitle"] == "Freespace":
+        points = obj["points"]["exterior"]
+        print(points)
+
+## draw filled polygon width, height, points 
+mask = np.zeros((1208, 1920, 3), np.uint8)
+pts = np.array(points, np.int32)
+pts = pts.reshape((-1,1,2))
+
+cv2.fillPoly(mask,[pts],(255,255,255))
+cv2.imshow('Window', mask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite(join(MASK_DIR,"asd.png"),mask)
