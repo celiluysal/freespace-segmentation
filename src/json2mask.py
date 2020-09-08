b9205af7c8f19f68fa3055694ec7256a969c5b68
celiluysal <celiluysl15@gmail.com>
2020-09-08 22:12:14 +0300

    clean type name method

--- src/json2mask.py
+++ src/json2mask.py
@@ -4,6 +4,7 @@ import numpy as np
 import json
 import os
 import cv2
+import re
         
 images = []
 JSON_DIR = "../data/jsons"
@@ -18,6 +19,11 @@ class image:
         self.points = []
         self.points.append(points)
         self.name = name
+        
+def clean_TypeName(_name):
+    pattern = r'\..*'
+    name = re.sub(pattern, '', _name)
+    return name
 
 for file_name in file_names:
     json_path = os.path.join(JSON_DIR, file_name)
@@ -32,11 +38,13 @@ for file_name in file_names:
         if obj["classTitle"] == "Freespace":
             fs_count += 1
             points = obj["points"]["exterior"]
-            img = image(width, height, points, file_name)
+            name = clean_TypeName(file_name)
+            img = image(width, height, points, name)
             if(fs_count > 1):
-                print(file_name)
+                print(name)
                 img.points.append(points)
     images.append(img)
+
             
 def draw_and_save_filledPolygon(_image:image):
     mask = np.zeros((_image.width, _image.height, 3), np.uint8)
@@ -47,7 +55,7 @@ def draw_and_save_filledPolygon(_image:image):
         pts = pts.reshape((-1,1,2))
         cv2.fillPoly(mask,[pts],(255,255,255))
     
-    cv2.imwrite(join(MASK_DIR,_image.name+".png"),mask)
+    cv2.imwrite(join(MASK_DIR,_image.name+"_mask.png"),mask)
 
 #for image in images:
 #    draw_and_save_filledPolygon(image)
--- src/test.py
+++ src/test.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 20:41:54 2020
+
+@author: celil
+"""
+
+
 from os import listdir
 from os.path import isfile, join
 import numpy as np
@@ -8,8 +16,7 @@ import cv2
 images = []
 JSON_DIR = "../data/jsons"
 MASK_DIR = "../data/masks"
-
-file_names = [f for f in listdir(JSON_DIR) if isfile(join(JSON_DIR, f))]
+# file_names = [f for f in listdir(JSON_DIR) if isfile(join(JSON_DIR, f))]
 
 class image:
     def __init__(self, width, height, points, name):
@@ -68,7 +75,43 @@ def draw_and_save_filledPolygon2(_image:image):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
-draw_and_save_filledPolygon2(im1)
+def draw_filledPolygon_on_image(_image:image):
+    png = cv2.imread("cfc_000234.jpg")
+    
+    copy_png = png.copy()
+    
+    points_list = _image.points
+    for points in points_list:
+        pts = np.array(points, np.int32)
+        pts = pts.reshape((-1,1,2))
+        white = (255,255,255)
+        green = (153,255,51)
+        purple = (204,0,255)
+        cv2.fillPoly(copy_png,[pts],green)
+        opacity = 0.4
+        cv2.addWeighted(copy_png,opacity,png,1-opacity,0,png)
+    #cv2.imwrite(join(MASK_DIR,_name+".png"),mask)
+    cv2.imshow("image",png)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+#draw_filledPolygon_on_image(im1) 
+    
+import re
+
+pattern = r'\..*'
+test_string = 'cfc_000235.png.json'
+result = re.search(pattern, test_string)
+result.group()
+print(result.group())
+
+name = 'cfc_000235.png.json.png'
+name = re.sub(pattern, '', name)
+print(name)
+
+    
+    
+# draw_and_save_filledPolygon2(im1)
     
 #draw_and_save_filledPolygon(200, 200, [[200,0],[200,100],[150,100],[150,0]],"image1")
 #draw_and_save_filledPolygon(200, 200, [[0,0],[0,100],[50,100],[50,0]],"image1")
@@ -77,4 +120,3 @@ draw_and_save_filledPolygon2(im1)
 
 #for image in images:
     #draw_and_save_filledPolygon(image.width, image.height, image.points, image.name)
-
