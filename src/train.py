73b36472e0dec9b918a6c5fe5d2c9393440e2c37
celiluysal <celiluysl15@gmail.com>
2020-10-09 17:02:25 +0300

    use test data for predict masks

--- src/train.py
+++ src/train.py
@@ -1,6 +1,10 @@
-from model import FoInternNet
-# from unet import UNet
+# from model import FoInternNet
+from unet import UNet
 
+from PIL import Image
+import cv2
+
+from mask_on_image import write_mask_on_image2
 from preprocess import tensorize_image,tensorize_mask, image_mask_check, decode_and_convert_image
 import os
 import glob, tqdm
@@ -13,8 +17,8 @@ import matplotlib.pyplot as plt
 ######### PARAMETERS ##########
 valid_size = 0.3
 test_size  = 0.1
-batch_size = 4
-epochs = 1
+batch_size = 8
+epochs = 35
 cuda = True
 input_shape = (224, 224)
 n_classes = 2
@@ -64,12 +68,14 @@ train_label_path_list = mask_path_list[valid_ind:]
 steps_per_epoch = len(train_input_path_list)//batch_size
 
 # CALL MODEL
-model = FoInternNet(input_size=input_shape, n_classes=2)
-# model = UNet(n_channels=3, n_classes=2, bilinear=True)
+# model = FoInternNet(input_size=input_shape, n_classes=2)
+model = UNet(n_channels=3, n_classes=2, bilinear=True)
 
 # DEFINE LOSS FUNCTION AND OPTIMIZER
+# criterion = nn.CrossEntropyLoss()
 criterion = nn.BCELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+# optimizer = optim.RMSprop(model.parameters(),lr=0.001, momentum=0.9)
 
 # IF CUDA IS USED, IMPORT THE MODEL INTO CUDA
 if cuda:
@@ -81,82 +87,99 @@ outputs_list2 = []
 
 for epoch in range(epochs):
     running_loss = 0
-    # print("epoch ", epoch)
-    # print("steps_per_epoch: {}",steps_per_epoch)
     for ind in tqdm.tqdm(range(steps_per_epoch)):
-        #472-476
         batch_input_path_list = train_input_path_list[batch_size*ind:batch_size*(ind+1)]
         batch_label_path_list = train_label_path_list[batch_size*ind:batch_size*(ind+1)]
+        
         batch_input = tensorize_image(batch_input_path_list, input_shape, cuda)
-        # batch_input = batch_input.to('cuda')
-
-
-        # print(batch_input_path_list)
-        # print("image shape: ", batch_input.shape)
-        # print(batch_input)
         batch_label = tensorize_mask(batch_label_path_list, input_shape, n_classes, cuda)
-        # batch_label = batch_label.to('cuda')
-        # print(batch_label_path_list)
-        # print("mask shape : ", batch_label.shape)
 
         optimizer.zero_grad()
 
-        # print(batch_label.shape)
-        # print(batch_label)
         outputs = model(batch_input)
-        test_output1 = outputs
-        # print("output1 ", test_output1.shape)
-        # if(epoch == 1):
-            # outputs_list1.append(outputs)
-        # print(outputs[0])
-        # print("2------------------------")
-        # print("outputs: ",outputs)
+
         loss = criterion(outputs, batch_label)
-        # print("2------------------------")
         loss.backward()
         optimizer.step()
 
         running_loss += loss.item()
-        # print(ind)
+
         if ind == steps_per_epoch-1:
             str1 = 'training loss on epoch   {}: {}'.format(epoch, running_loss)
-            print(str1)
-
             val_loss = 0
             for (valid_input_path, valid_label_path) in zip(valid_input_path_list, valid_label_path_list):
                 batch_input = tensorize_image([valid_input_path], input_shape, cuda)
                 batch_label = tensorize_mask([valid_label_path], input_shape, n_classes, cuda)
+                
                 outputs = model(batch_input)
-                test_output2 = outputs
-                # print("output2 ", test_output2.shape)
-                # if(epoch == 1):
-                    # outputs_list2.append(outputs)
 
                 loss = criterion(outputs, batch_label)
                 val_loss += loss
                 break
-            
             str2 = 'validation loss on epoch {}: {}'.format(epoch, val_loss)
-            print(str2)
+    print(str1)
+    print(str2)
 
     
     if(epoch == epochs-1):
         torch.cuda.empty_cache()
-        test_output_list = []
+        predict_mask_list = []
         for test_input_path, test_label_path in zip(test_input_path_list, test_label_path_list):
             batch_input = tensorize_image([test_input_path], input_shape, cuda)
-            # batch_label = tensorize_mask([test_label_path], input_shape, n_classes, cuda)
+            batch_label = tensorize_mask([test_label_path], input_shape, n_classes, cuda)
             # print("1----")
             outputs = model(batch_input)
             # print("2----")
-            test_output_list.append(outputs)
+
+            c = outputs > 0.5
+            d = decode_and_convert_image(c, n_class=2)
+            e = d[0]
+            # mask = Image.fromarray((e * 255).astype(np.uint8))
+            # print(e)
+            predict_mask_list.append(e)
+            
+
+        write_mask_on_image2(predict_mask_list, test_input_path_list, input_shape)
+
+
+
+            # print("append")
 
 
 # img_list = decode_and_convert_image(outputs, n_classes)
 # img = img_list[0]
 
-print(len(test_output_list))
-print(test_output_list[0])
+# print(len(test_output1_list))
+# for i in range(len(test_output1_list)):
+    # a = test_output1_list[i]
+    # b = a
+    # # b = torch.softmax(a, dim=1)
+    # # b = b.squeeze(0)
+
+    # c = b > 0.5
+    # d = decode_and_convert_image(c, n_class=2)
+    # e = d[0]
+    # img = Image.fromarray((e * 255).astype(np.uint8))
+
+    # mask = cv2.imread(test_label_path_list[i], 0)
+    # mask = cv2.resize(mask,input_shape)
+
+    # image = cv2.imread(test_input_path_list[i], cv2.IMREAD_COLOR)
+    # image = cv2.resize(image,input_shape)
+
+    # f = plt.figure()
+    # f.add_subplot(1,3, 1)
+    # plt.imshow(a, cmap="gray")
+    # f.add_subplot(1,3, 2)
+    # plt.imshow(mask, cmap="gray")
+    # f.add_subplot(1,3, 3)
+    # plt.imshow(image, cmap="gray")
+    # plt.show()
+
+
+# plt.imshow(b, cmap="gray")
+# plt.show()
+
 
 # print(test_output1.shape)
 # print(test_output1)
@@ -166,4 +189,6 @@ print(test_output_list[0])
 # print(img)
 # plt.imshow(img, cmap="gray")
 # plt.show()
+
+
 
