33bf36184fb0aaae3944846430f43d57a595eb83
celiluysal <celiluysl15@gmail.com>
2020-10-13 20:36:05 +0300

    added load, save and ru model

--- src/train.py
+++ src/train.py
@@ -1,28 +1,25 @@
 from unet import UNet
-
-from PIL import Image
-import cv2
-
 from mask_on_image import write_mask_on_image2
 from preprocess import tensorize_image,tensorize_mask, image_mask_check, decode_and_convert_image
-import os
-from os.path import isfile, join
-import glob, tqdm
+from data_utils import draw_loss_graph, norm
+
+from os.path import join
+import os, glob, tqdm, cv2, torch
 import numpy as np
-import torch
 import torch.nn as nn
 import torch.optim as optim
-import matplotlib.pyplot as plt
+
 
 ######### PARAMETERS ##########
 valid_size = 0.3
 test_size  = 0.1
 batch_size = 8
-epochs = 2
+epochs = 20
 cuda = True
 augmentation = True
 test_predict = True
-predict_save_file_name = "1"
+predict_save_file_name = "test3"
+model_file_name = predict_save_file_name
 input_shape = (224, 224)
 n_classes = 2
 ###############################
@@ -35,6 +32,7 @@ DATA_DIR = os.path.join(ROOT_DIR, 'data')
 # MASK_DIR = os.path.join(DATA_DIR, 'masks')
 IMAGE_DIR = os.path.join(DATA_DIR, 'test_images')
 MASK_DIR = os.path.join(DATA_DIR, 'test_masks')
+MODEL_DIR = os.path.join(DATA_DIR, 'models')
 ##############################
 
 # PREPARE IMAGE AND MASK LISTS
@@ -86,94 +84,92 @@ if cuda:
 run_loss_list = list()
 val_loss_list = list()
 
-for epoch in range(epochs):
-    running_loss = 0
-    for ind in tqdm.tqdm(range(steps_per_epoch)):
-        batch_input_path_list = train_input_path_list[batch_size*ind:batch_size*(ind+1)]
-        batch_label_path_list = train_label_path_list[batch_size*ind:batch_size*(ind+1)]
-        
-        batch_input = tensorize_image(batch_input_path_list, input_shape, cuda, epoch%2==0 if augmentation else False)
-        # batch_input = tensorize_image(batch_input_path_list, input_shape, cuda)
-        batch_label = tensorize_mask(batch_label_path_list, input_shape, n_classes, cuda)
+def train():
+    for epoch in range(epochs):
+        running_loss = 0
+        for ind in tqdm.tqdm(range(steps_per_epoch)):
+            batch_input_path_list = train_input_path_list[batch_size*ind:batch_size*(ind+1)]
+            batch_label_path_list = train_label_path_list[batch_size*ind:batch_size*(ind+1)]
+            
+            # batch_input = tensorize_image(batch_input_path_list, input_shape, cuda, epoch%2==0 if augmentation else False)
+            batch_input = tensorize_image(batch_input_path_list, input_shape, cuda, augmentation)
+            batch_label = tensorize_mask(batch_label_path_list, input_shape, n_classes, cuda)
 
-        optimizer.zero_grad()
+            optimizer.zero_grad()
 
-        outputs = model(batch_input)
+            outputs = model(batch_input)
 
-        loss = criterion(outputs, batch_label)
-        loss.backward()
-        optimizer.step()
+            loss = criterion(outputs, batch_label)
+            loss.backward()
+            optimizer.step()
 
-        running_loss += loss.item()
+            running_loss += loss.item()
 
-        if ind == steps_per_epoch-1:
-            run_loss_list.append(running_loss)
-            str1 = 'training loss on epoch   {}: {}'.format(epoch, running_loss)
-            val_loss = 0
-            for (valid_input_path, valid_label_path) in zip(valid_input_path_list, valid_label_path_list):
-                batch_input = tensorize_image([valid_input_path], input_shape, cuda)
-                batch_label = tensorize_mask([valid_label_path], input_shape, n_classes, cuda)
-                
-                outputs = model(batch_input)
+            if ind == steps_per_epoch-1:
+                run_loss_list.append(running_loss)
+                str1 = 'training loss on epoch   {}: {}'.format(epoch, running_loss)
+                val_loss = 0
+                for (valid_input_path, valid_label_path) in zip(valid_input_path_list, valid_label_path_list):
+                    batch_input = tensorize_image([valid_input_path], input_shape, cuda)
+                    batch_label = tensorize_mask([valid_label_path], input_shape, n_classes, cuda)
+                    
+                    outputs = model(batch_input)
 
-                loss = criterion(outputs, batch_label)
-                loss.backward()
-                val_loss += loss.item()
+                    loss = criterion(outputs, batch_label)
+                    loss.backward()
+                    val_loss += loss.item()
 
-            val_loss_list.append(val_loss)
-            str2 = 'validation loss on epoch {}: {}'.format(epoch, val_loss)
-    print(str1)
-    print(str2)
+                val_loss_list.append(val_loss)
+                str2 = 'validation loss on epoch {}: {}'.format(epoch, val_loss)
+        print(str1)
+        print(str2)
 
-if test_predict:
-    torch.cuda.empty_cache()
-    predict_mask_list = []
-    for test_input_path, test_label_path in tqdm.tqdm(zip(test_input_path_list, test_label_path_list)):
-        batch_input = tensorize_image([test_input_path], input_shape, cuda)
-        batch_label = tensorize_mask([test_label_path], input_shape, n_classes, cuda)
-                
-        outputs = model(batch_input)
+    norm_run_loss_list = norm(run_loss_list)
+    norm_val_loss_list = norm(val_loss_list)
 
-        label = outputs > 0.5
-        decoded_list = decode_and_convert_image(label, n_class=2)
-        mask = decoded_list[0]
-        predict_mask_list.append(mask)
-                
-        write_mask_on_image2(predict_mask_list, test_input_path_list, input_shape, predict_save_file_name)
+    graph_name = predict_save_file_name
+    draw_loss_graph(epochs, norm_run_loss_list, norm_val_loss_list, graph_name)
 
-epoch_list = list()
-for i in range(epochs):
-    epoch_list.append(i)
+def test(_model:UNet):    
+    if test_predict:
+        torch.cuda.empty_cache()
+        predict_mask_list = []
+        for test_input_path, test_label_path in tqdm.tqdm(zip(test_input_path_list, test_label_path_list)):
+            batch_input = tensorize_image([test_input_path], input_shape, cuda)
+            # batch_label = tensorize_mask([test_label_path], input_shape, n_classes, cuda)
+                    
+            outputs = _model(batch_input)
+
+            label = outputs > 0.5
+            decoded_list = decode_and_convert_image(label, n_class=2)
+            mask = decoded_list[0]
+            predict_mask_list.append(mask)
+                    
+            write_mask_on_image2(predict_mask_list, test_input_path_list, input_shape, predict_save_file_name)
+
+def save_model(model, model_name):
+    if not os.path.exists(MODEL_DIR):
+        os.mkdir(MODEL_DIR)
+    model_name += ".pt"
+    torch.save(model,join(MODEL_DIR, model_name))
+
+def load_model(model_dir):
+    loaded_model = torch.load(model_dir)
+    loaded_model.eval()
+    return loaded_model
+
+if __name__ == "__main__":
+    train()
+    save_model(model, model_file_name)
+    test(model)
+    
+    print("test is done. Press any key for load and test model")
+    input()
+
+    predict_save_file_name += "with_load"
+
+    loaded_model = load_model(join(MODEL_DIR, model_file_name + ".pt"))
+    test(loaded_model)
 
 
-def draw_loss_graph(epoch_list, run_loss_list, val_loss_list, save_file_name):
-    save_file_name = "../data/predicts/" + save_file_name
-    if not os.path.exists(save_file_name):
-        os.mkdir(save_file_name)
 
-    fig = plt.figure()
-    fig.add_subplot(221)
-    plt.plot(epoch_list, run_loss_list, label = "training loss", color="C0") 
-    plt.tick_params(labelsize=7)
-    plt.title("training loss", fontsize=7)
-
-    fig.add_subplot(222)
-    plt.plot(epoch_list, val_loss_list, label = "validation loss", color="C1") 
-    plt.tick_params(labelsize=7)
-    plt.title("validation loss", fontsize=7)
-
-    fig.add_subplot(212)
-    plt.plot(epoch_list, run_loss_list, label = "training loss", color="C0") 
-    plt.plot(epoch_list, val_loss_list, label = "validation loss", color="C1") 
-    plt.tick_params(labelsize=7)
-    plt.legend()
-
-    plt.savefig(join(save_file_name, "graph.png")) 
-
-def norm(raw):
-    return [float(i)/sum(raw) for i in raw]
-
-run_loss_list = norm(run_loss_list)
-val_loss_list = norm(val_loss_list)
-
-draw_loss_graph(epoch_list, run_loss_list, val_loss_list, predict_save_file_name)
